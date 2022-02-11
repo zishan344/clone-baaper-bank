523dfe860c6abf29cfe687dff79b3893ffe4b55d
zishan344 <zishanahmed344@gmail.com>
2022-02-11 17:54:42 +0600

    done the function add programm

--- js/banking.js
+++ js/banking.js
@@ -1,37 +1,55 @@
-document.getElementById("deposit-btn").addEventListener("click", function () {
-  const depositInput = document.getElementById("deposit-input");
-  const depositAmount = parseFloat(depositInput.value);
+function getInput(inputId) {
+  const totalAmountInput = document.getElementById(inputId);
+  const totalAmountTExt = parseFloat(totalAmountInput.value);
+  // clear input field
+  totalAmountInput.value = "";
+  return totalAmountTExt;
+}
 
-  const depositTotal = document.getElementById("deposit-total");
-  const previousDeposit = parseFloat(depositTotal.innerText);
-
-  const totalDeposit = previousDeposit + depositAmount;
-  depositTotal.innerText = totalDeposit;
-
-  // update account balance
+function updateField(inputField, amount) {
+  const totalElement = document.getElementById(amount);
+  const totalText = parseFloat(totalElement.innerText);
+  const previousTotal = totalText + inputField;
+  totalElement.innerText = previousTotal;
+}
+function totalBalance(amount, isAdd) {
+  const Balance = document.getElementById("balance-total");
+  const previousBalance = getcurentBalance();
+  if (isAdd == true) {
+    const totalBalance = previousBalance + amount;
+    Balance.innerText = totalBalance;
+  } else {
+    const totalBalance = previousBalance - amount;
+    Balance.innerText = totalBalance;
+  }
+}
+function getcurentBalance() {
   const Balance = document.getElementById("balance-total");
   const previousBalance = parseFloat(Balance.innerText);
-  const currentDeposit = parseFloat(depositTotal.innerText);
-  const totalBalance = previousBalance + currentDeposit;
-  Balance.innerText = totalBalance;
-  // clear input field
-  depositInput.value = "";
+  return previousBalance;
+}
+
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  const depositAmount = getInput("deposit-input");
+  if (depositAmount > 0) {
+    //updateField
+    updateField(depositAmount, "deposit-total");
+    // update account balance
+    totalBalance(depositAmount, true);
+  } else {
+    alert("enter a valid number");
+  }
 });
 
 document.getElementById("withdrow-btn").addEventListener("click", function () {
-  // console.log("all is done");
-  const withdrowInput = document.getElementById("withdrow-input");
-  const withdrowInputAmount = parseFloat(withdrowInput.value);
-  const withdrowTotal = document.getElementById("withdrow-total");
-  const withdrowAmount = parseFloat(withdrowTotal.innerText);
-  const totalWithdrowAmount = withdrowInputAmount + withdrowAmount;
-  withdrowTotal.innerText = totalWithdrowAmount;
-
-  // update balance status
-  const totalBalance = document.getElementById("balance-total");
-  const totalBalanceupdate = parseFloat(totalBalance.innerText);
-  const totalUpadatedBalance = totalBalanceupdate - withdrowInputAmount;
-  totalBalance.innerText = totalUpadatedBalance;
-  // clear the input field
-  withdrowInput.value = "";
+  const withdrowInputAmount = getInput("withdrow-input");
+  const cureentBalance = getcurentBalance();
+  if (withdrowInputAmount > 0 && withdrowInputAmount < cureentBalance) {
+    // updateField
+    updateField(withdrowInputAmount, "withdrow-total");
+    // update balance status
+    totalBalance(withdrowInputAmount, false);
+  } else {
+    alert("enter a valid number");
+  }
 });
